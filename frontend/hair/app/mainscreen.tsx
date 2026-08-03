9273e73de97397f30246ff12b4a9701a8304d08a
Dmitri Power <160439126+xSantanxx@users.noreply.github.com>
2026-08-03 15:52:29 -0400

    add animation

--- frontend/hair/app/mainscreen.tsx
+++ frontend/hair/app/mainscreen.tsx
@@ -1,6 +1,6 @@
 import { useNavigation, useRoute } from "@react-navigation/native";
-import { View, Text, StyleSheet, Pressable, Image, ScrollView } from "react-native";
-import { useState } from "react";
+import { View, Text, StyleSheet, Pressable, Image, ScrollView, LayoutAnimation } from "react-native";
+import { useRef, useState } from "react";
 import { GlassView } from "expo-glass-effect";
 
 export default function MainScreen(){
@@ -10,6 +10,23 @@ export default function MainScreen(){
     const {name, hairType, hairTypeImage} = navigation.params || {};
 
     const [activeTab, setActiveTab] = useState<string>('porosity');
+    const [stretch, setStretch] = useState(false);
+    const isRunning = useRef<number | null>(null);
+
+    const startTimer = () => {
+        LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+        setStretch(true);
+        if(isRunning.current !== null) {
+            clearTimeout(isRunning.current);
+        };
+
+        isRunning.current = setTimeout(() => {
+            setStretch(false);
+            LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+            isRunning.current = null;
+        }, 5000);
+
+    }
 
     return (
         <View style={styles.container}>
@@ -22,12 +39,12 @@ export default function MainScreen(){
                 <Text style={styles.text}>{hairType}🧑‍🦳</Text>
             </View>
             <View style={styles.box5}>
-            <GlassView glassEffectStyle="clear" tintColor="lightblue" style={styles.glassView}>
+            <GlassView glassEffectStyle="clear" tintColor="lightblue" style={[styles.glassView, stretch && styles.stretch]}>
                 <ScrollView horizontal={true}>
-                    <Pressable onPress={() => setActiveTab('porosity')} style={[styles.button, activeTab === 'porosity' && styles.buttonAct]}><Text style={styles.text}>Porosity Test🧑‍🦳</Text></Pressable>
-                    <Pressable onPress={() => setActiveTab('scalp')} style={[styles.button, activeTab === 'scalp' && styles.buttonAct]}><Text style={styles.text}>Scalp Help🧑‍🦳</Text></Pressable>
-                    <Pressable onPress={() => setActiveTab('hairType')} style={[styles.button, activeTab === 'hairType' && styles.buttonAct]}><Text style={styles.text}>Hair Type🧑‍🦳</Text></Pressable>
-                    <Pressable onPress={() => setActiveTab('care')} style={[styles.button, activeTab === 'care' && styles.buttonAct]}><Text style={styles.text}>Hair Type🧑‍🦳</Text></Pressable>
+                    <Pressable onPress={() => {setActiveTab('porosity'); startTimer()}} style={[styles.button, activeTab === 'porosity' && styles.buttonAct]}><Text style={styles.text}>Porosity Test🧑‍🦳</Text></Pressable>
+                    <Pressable onPress={() => {setActiveTab('scalp'); startTimer()}} style={[styles.button, activeTab === 'scalp' && styles.buttonAct]}><Text style={styles.text}>Scalp Help🧑‍🦳</Text></Pressable>
+                    <Pressable onPress={() => {setActiveTab('hairType'); startTimer()}} style={[styles.button, activeTab === 'hairType' && styles.buttonAct]}><Text style={styles.text}>Hair Type🧑‍🦳</Text></Pressable>
+                    <Pressable onPress={() => {setActiveTab('care'); startTimer()}} style={[styles.button, activeTab === 'care' && styles.buttonAct]}><Text style={styles.text}>Hair Type🧑‍🦳</Text></Pressable>
                 </ScrollView>
                 </GlassView>
             </View>
@@ -88,6 +105,7 @@ const styles = StyleSheet.create({
         borderRadius: 16,
         minHeight: 10,
         marginRight: 5,
+        overflow: 'hidden',
     },
     buttonAct: {
         backgroundColor: 'rgba(255, 255, 255, 0.45)',
@@ -101,9 +119,23 @@ const styles = StyleSheet.create({
     },
     glassView: {
         justifyContent: 'space-between',
+        // alignItems: 'center',
+        alignSelf: 'center',
+        width: '33%',
+        padding: 2,
+        flexDirection: 'row',
+        height: 40,
+        borderRadius: 16,
+    },
+    stretch: {
         alignItems: 'center',
+        justifyContent: 'space-between',
+        width: '100%',
         padding: 10,
         flexDirection: 'row',
+        borderRadius: 16,
+        height: 50,
+
     },
     box5: {
         position: 'absolute',
